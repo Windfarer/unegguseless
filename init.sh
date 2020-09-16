926c595601ae706728f80b38bcf5b5ec01004130
Windfarer <windfarer@gmail.com>
2020-09-16 14:18:31 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -20,7 +20,7 @@ sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 sudo usermod -aG docker $USER
-sudo systemd enable docker
+sudo systemctl enable docker
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
