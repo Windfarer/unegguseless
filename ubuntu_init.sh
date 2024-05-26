7c6e88e2758fe7bf079e4213c2cf5cf96b0885f1
Windfarer <windfarer@gmail.com>
2024-05-26 16:38:15 +0800

    Update ubuntu_init.sh

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -21,6 +21,7 @@ sudo usermod -aG docker $USER
 wget http://url-for/NVIDIA-Linux-x86_64-550.54.15.run
 
 chmod +x NVIDIA-Linux-x86_64-550.54.15.run 
+sudo ./NVIDIA-Linux-x86_64-550.54.15.run
 
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list |     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' |     sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
